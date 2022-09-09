2b6b74271b7947d887061f0461e160075b2739cf
Philipp Guertler <phili.gue@hotmail.de>
2022-09-09 13:09:36 +0200

    added a games screen

---
+++ pile_of_shame/lib/src/screens/games.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class GameScreen extends StatefulWidget {
+  const GameScreen({super.key});
+
+  @override
+  State<GameScreen> createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) {
+          return const Divider();
+        }
+        return const ListTile(title: Text('TODO: Display Game entries'));
+      },
+    );
+  }
+}
