58c64a919af07ac8ff7d0b35bc4d9e754c6abd1b
Philipp Guertler <phili.gue@hotmail.de>
2022-09-09 18:03:14 +0200

    added commas for auto-format

--- pile_of_shame/lib/src/widgets/age_restriction.dart
+++ pile_of_shame/lib/src/widgets/age_restriction.dart
@@ -10,34 +10,44 @@ class AgeRestrictionWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-        width: 60,
-        height: 60,
-        child: ClipRRect(
-            borderRadius: BorderRadius.circular(8.0),
-            child: ColoredBox(
-                color: game.getAgeRestictionColor().withOpacity(0.5),
-                child: Center(
+      width: 60,
+      height: 60,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(8.0),
+        child: ColoredBox(
+          color: game.getAgeRestictionColor().withOpacity(0.5),
+          child: Center(
+            child: RotationTransition(
+              turns: AlwaysStoppedAnimation(1 / 8),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(8.0),
+                child: SizedBox(
+                  width: 40,
+                  height: 40,
+                  child: ColoredBox(
+                    color: game.getAgeRestictionColor(),
                     child: RotationTransition(
-                        turns: AlwaysStoppedAnimation(1 / 8),
-                        child: ClipRRect(
-                            borderRadius: BorderRadius.circular(8.0),
-                            child: SizedBox(
-                                width: 40,
-                                height: 40,
-                                child: ColoredBox(
-                                    color: game.getAgeRestictionColor(),
-                                    child: RotationTransition(
-                                        turns: AlwaysStoppedAnimation(-1 / 8),
-                                        child: Center(
-                                            child: Text(
-                                                game.getAgeRestrictionText(),
-                                                style: TextStyle(
-                                                    color: game
-                                                                .getAgeRestictionColor()
-                                                                .computeLuminance() >
-                                                            0.5
-                                                        ? Colors.black
-                                                        : Colors
-                                                            .white))))))))))));
+                      turns: AlwaysStoppedAnimation(-1 / 8),
+                      child: Center(
+                        child: Text(
+                          game.getAgeRestrictionText(),
+                          style: TextStyle(
+                              color: game
+                                          .getAgeRestictionColor()
+                                          .computeLuminance() >
+                                      0.5
+                                  ? Colors.black
+                                  : Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
