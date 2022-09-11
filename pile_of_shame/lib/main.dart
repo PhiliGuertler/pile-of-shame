bc24db29cd771933f5821001a11738aedb775f97
Philipp Guertler <phili.gue@hotmail.de>
2022-09-11 17:37:01 +0200

    added intl as a dependency

--- pile_of_shame/lib/main.dart
+++ pile_of_shame/lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
-
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
 import 'src/screens/games.dart';
 
 void main() {
-  runApp(const PileOfShameApp());
+  Intl.defaultLocale = 'de_DE';
+  initializeDateFormatting('de_DE', null).then(
+    (value) {
+      runApp(const PileOfShameApp());
+    },
+  );
 }
 
 class PileOfShameApp extends StatelessWidget {
@@ -12,7 +18,7 @@ class PileOfShameApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Pile of Shame',
       theme: ThemeData.dark(),
       home: const GameScreen(),
     );
