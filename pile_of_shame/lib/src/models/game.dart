7a45bcad760a12d8750fd898e4417dd7cfd632bb
Philipp Guertler <phili.gue@hotmail.de>
2022-09-11 13:13:20 +0200

    added isFavourite flag

--- pile_of_shame/lib/src/models/game.dart
+++ pile_of_shame/lib/src/models/game.dart
@@ -2,14 +2,25 @@ import 'package:flutter/material.dart';
 import 'age_restrictions.dart';
 
 class Game {
-  String platform;
+  /// the game's title
   String title;
+
+  /// the platform(s) on which the game was purchased/played
+  String platform;
+
+  /// the amount of money that was actually payed for the game
   double? price;
+
+  /// the game's age restriction
   AgeRestriction? ageRestriction;
+
+  /// signifies if the game is a favourite game or not
+  bool isFavourite;
 
   Game(
       {required this.platform,
       required this.title,
+      this.isFavourite = false,
       this.price,
       this.ageRestriction});
 
