eecdc491af53745293c8f8c4de871d013734fa9b
Philipp Guertler <phili.gue@hotmail.de>
2022-09-11 16:52:02 +0200

    updated details to display an image

--- pile_of_shame/lib/src/screens/game_details.dart
+++ pile_of_shame/lib/src/screens/game_details.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:pile_of_shame/src/network/rawg/rawg_api.dart';
 import 'package:pile_of_shame/src/widgets/game_list_item.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 import '../models/game.dart';
 
-class GameDetails extends StatelessWidget {
+class GameDetails extends StatefulWidget {
   const GameDetails({super.key, required this.game});
 
   final Game game;
+
+  @override
+  State<GameDetails> createState() => _GameDetailsState();
+}
+
+class _GameDetailsState extends State<GameDetails> {
+  late Future<Iterable<RawgGame>> gameInfos;
+
+  @override
+  void initState() {
+    super.initState();
+    // Fetch game info here
+    gameInfos = RawgApi().searchGameByName(widget.game.title);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,12 +30,46 @@ class GameDetails extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Details'),
       ),
-      body: Container(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(children: [
-          GameListItem(game: game),
-        ]),
-      ),
+      body: Column(children: [
+        FutureBuilder<Iterable<RawgGame>>(
+          future: gameInfos,
+          builder: ((context, snapshot) {
+            return Stack(
+              children: [
+                ShaderMask(
+                  shaderCallback: (bounds) {
+                    return const LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [Colors.black, Colors.transparent],
+                      stops: [0.6, 1.0],
+                    ).createShader(
+                        Rect.fromLTRB(0, 0, bounds.width, bounds.height));
+                  },
+                  blendMode: BlendMode.dstIn,
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: 250,
+                    child: snapshot.hasData
+                        ? FadeInImage.memoryNetwork(
+                            fadeInDuration: const Duration(seconds: 1),
+                            placeholder: kTransparentImage,
+                            image: snapshot.data!.first.backgroundImage,
+                            fit: BoxFit.fitHeight,
+                          )
+                        : null,
+                    // if (snapshot.hasError) Text('${snapshot.error}'),
+                  ),
+                ),
+              ],
+            );
+          }),
+        ),
+        Container(
+          padding: const EdgeInsets.all(8.0),
+          child: GameListItem(game: widget.game),
+        ),
+      ]),
     );
   }
 }
