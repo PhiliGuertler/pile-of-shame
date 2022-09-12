ad55d8470a27bfb29f145532d04dedc7f68b7102
Philipp Guertler <phili.gue@hotmail.de>
2022-09-12 22:20:26 +0200

    added a persistant storage provider for games

---
+++ pile_of_shame/lib/src/persistance/games_storage.dart
@@ -0,0 +1,45 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:path_provider/path_provider.dart';
+
+import '../models/game.dart';
+
+class GamesStorage {
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    debugPrint('LocalPath: ${directory.toString()}');
+
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/games.json');
+  }
+
+  Future<List<Game>> readGames() async {
+    try {
+      // Get the file
+      final file = await _localFile;
+      // Read it
+      final contents = await file.readAsString();
+      // decode the json contents to a List of Games
+      return jsonDecode(contents);
+    } catch (error) {
+      debugPrint(
+          'An error occured while reading the file: ${error.toString()}');
+      // return an empty list
+      return <Game>[];
+    }
+  }
+
+  Future<File> writeGames(List<Game> games) async {
+    // Get the file
+    final file = await _localFile;
+    // write the games as a json
+    return file.writeAsString(jsonEncode(games));
+  }
+}
