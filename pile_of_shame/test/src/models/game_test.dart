b235c53c66817d932d596cc5675af52b33ac8bbe
Philipp Guertler <phili.gue@hotmail.de>
2022-09-15 00:50:52 +0200

    game conversion tests are now running again

--- pile_of_shame/test/src/models/game_test.dart
+++ pile_of_shame/test/src/models/game_test.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:pile_of_shame/src/models/age_restrictions.dart';
 import 'package:test/test.dart';
 import 'package:pile_of_shame/src/models/game.dart';
@@ -8,16 +6,18 @@ void main() {
   group('Game', () {
     test('JSON encoding works as intended', () {
       final Game game = Game(
-          title: 'Test-Game',
-          platform: 'PC',
-          ageRestriction: AgeRestriction.usk12,
-          backgroundImage: 'https://some.url',
-          isFavourite: false,
-          metacriticScore: 99,
-          price: 91.64,
-          rawgGameId: 1234,
-          releaseDate: DateTime.parse('1999-01-02'),
-          wasScraped: true);
+        title: 'Test-Game',
+        platforms: ['PC', 'Nintendo Switch'],
+        ageRestriction: AgeRestriction.usk12,
+        backgroundImage: 'https://some.url',
+        isFavourite: false,
+        metacriticScore: 99,
+        price: 91.64,
+        rawgGameId: 1234,
+        releaseDate: DateTime.parse('1999-01-02'),
+        wasScraped: true,
+        notes: 'These are some noteworthy notes',
+      );
 
       final result = game.toJson();
 
@@ -25,7 +25,7 @@ void main() {
         result,
         {
           'title': 'Test-Game',
-          'platform': 'PC',
+          'platforms': ['PC', 'Nintendo Switch'],
           'ageRestriction': 3,
           'backgroundImage': 'https://some.url',
           'isFavourite': false,
@@ -34,13 +34,14 @@ void main() {
           'rawgGameId': 1234,
           'releaseDate': '1999-01-02T00:00:00.000',
           'wasScraped': true,
+          'notes': 'These are some noteworthy notes',
         },
       );
     });
     test('JSON decoding works as intended', () {
       final jsonMap = {
         'title': 'Test-Game',
-        'platform': 'PC',
+        'platforms': ['PC', 'Nintendo Switch'],
         'ageRestriction': 3,
         'backgroundImage': 'https://some.url',
         'isFavourite': false,
@@ -49,19 +50,22 @@ void main() {
         'rawgGameId': 1234,
         'releaseDate': '1999-01-02T00:00:00.000',
         'wasScraped': true,
+        'notes': 'These are some noteworthy notes',
       };
 
       final Game expectedGame = Game(
-          title: 'Test-Game',
-          platform: 'PC',
-          ageRestriction: AgeRestriction.usk12,
-          backgroundImage: 'https://some.url',
-          isFavourite: false,
-          metacriticScore: 99,
-          price: 91.64,
-          rawgGameId: 1234,
-          releaseDate: DateTime.parse('1999-01-02'),
-          wasScraped: true);
+        title: 'Test-Game',
+        platforms: ['PC', 'Nintendo Switch'],
+        ageRestriction: AgeRestriction.usk12,
+        backgroundImage: 'https://some.url',
+        isFavourite: false,
+        metacriticScore: 99,
+        price: 91.64,
+        rawgGameId: 1234,
+        releaseDate: DateTime.parse('1999-01-02'),
+        wasScraped: true,
+        notes: 'These are some noteworthy notes',
+      );
 
       final result = Game.fromJson(jsonMap);
 
