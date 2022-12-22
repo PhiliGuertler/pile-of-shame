f807cdf0e082c7df80cb4a374d404faaeb638114
Philipp Guertler <phili.gue@hotmail.de>
2022-12-22 18:10:02 +0100

    added firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:pile_of_shame/firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
