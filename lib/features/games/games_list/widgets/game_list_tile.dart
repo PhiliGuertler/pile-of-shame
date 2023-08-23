b45284c4155a1818b49ce40a70f17d86dfc8a7e0
Philipp Guertler <phili.gue@hotmail.de>
2023-08-23 13:39:04 +0200

    added routing to game list tile and altered its appearance

--- lib/features/games/games_list/widgets/game_list_tile.dart
+++ lib/features/games/games_list/widgets/game_list_tile.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pile_of_shame/features/games/game_details/screens/game_details_screen.dart';
 import 'package:pile_of_shame/models/game.dart';
 import 'package:pile_of_shame/providers/format_provider.dart';
 import 'package:pile_of_shame/widgets/game_platform_icon.dart';
+import 'package:pile_of_shame/widgets/play_status_display.dart';
 
 class GameListTile extends ConsumerWidget {
   final Game game;
@@ -14,17 +16,32 @@ class GameListTile extends ConsumerWidget {
     final currencyFormatter = ref.watch(currencyFormatProvider(context));
     final dateFormatter = ref.watch(dateFormatProvider);
 
-    return ListTile(
-      title: Text(game.name),
-      leading: GamePlatformIcon(
-        platform: game.platform,
-      ),
-      trailing: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          Text(currencyFormatter.format(game.fullPrice())),
-          Text(dateFormatter.format(game.lastModified)),
-        ],
+    return DefaultTextStyle.merge(
+      style: TextStyle(color: game.status.toForegroundColor(context)),
+      child: ListTile(
+        tileColor: game.status.toBackgroundColor(context),
+        title: Text(game.name),
+        subtitle: PlayStatusDisplay(playStatus: game.status),
+        leading: GamePlatformIcon(
+          platform: game.platform,
+        ),
+        trailing: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(currencyFormatter.format(game.fullPrice())),
+            Text(dateFormatter.format(game.lastModified)),
+          ],
+        ),
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => GameDetailsScreen(
+                gameId: game.id,
+              ),
+            ),
+          );
+        },
       ),
     );
   }
