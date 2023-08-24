0927b351950761df07412c668ac035157a02a9d8
Philipp Guertler <phili.gue@hotmail.de>
2023-08-24 18:34:07 +0200

    replaced image assets with compressed images

--- lib/models/game_platforms.dart
+++ lib/models/game_platforms.dart
@@ -27,56 +27,56 @@ enum GamePlatform {
   // ### Handhelds ########################################################## //
   gameAndWatch(
     family: GamePlatformFamily.nintendo,
-    assetPath: "game_and_watch.png",
+    assetPath: "game_and_watch",
     name: "Game & Watch",
     abbreviation: "Game & Watch",
     type: GamePlatformType.handheld,
   ),
   gameBoy(
     family: GamePlatformFamily.nintendo,
-    assetPath: "game_boy.png",
+    assetPath: "game_boy",
     name: "GameBoy",
     abbreviation: "GB",
     type: GamePlatformType.handheld,
   ),
   gameBoyColor(
     family: GamePlatformFamily.nintendo,
-    assetPath: "game_boy_color.png",
+    assetPath: "game_boy_color",
     name: "GameBoy Color",
     abbreviation: "GBC",
     type: GamePlatformType.handheld,
   ),
   gameBoyAdvance(
     family: GamePlatformFamily.nintendo,
-    assetPath: "game_boy_advance.png",
+    assetPath: "game_boy_advance",
     name: "GameBoy Advance",
     abbreviation: "GBA",
     type: GamePlatformType.handheld,
   ),
   nintendoDS(
     family: GamePlatformFamily.nintendo,
-    assetPath: "nintendo_ds.png",
+    assetPath: "nintendo_ds",
     name: "Nintendo DS",
     abbreviation: "NDS",
     type: GamePlatformType.handheld,
   ),
   nintendoDSi(
     family: GamePlatformFamily.nintendo,
-    assetPath: "nintendo_dsi.png",
+    assetPath: "nintendo_dsi",
     name: "Nintendo DSi",
     abbreviation: "DSi",
     type: GamePlatformType.handheld,
   ),
   nintendo3DS(
     family: GamePlatformFamily.nintendo,
-    assetPath: "nintendo_3ds.png",
+    assetPath: "nintendo_3ds",
     name: "Nintendo 3DS",
     abbreviation: "3DS",
     type: GamePlatformType.handheld,
   ),
   newNintendo3DS(
     family: GamePlatformFamily.nintendo,
-    assetPath: "new_nintendo_3ds.png",
+    assetPath: "new_nintendo_3ds",
     name: "new Nintendo 3DS",
     abbreviation: "new 3DS",
     type: GamePlatformType.handheld,
@@ -84,49 +84,49 @@ enum GamePlatform {
   // ### Stationaries ####################################################### //
   nes(
     family: GamePlatformFamily.nintendo,
-    assetPath: "nes.png",
+    assetPath: "nes",
     name: "Nintendo Entertainment System",
     abbreviation: "NES",
     type: GamePlatformType.stationary,
   ),
   snes(
     family: GamePlatformFamily.nintendo,
-    assetPath: "super_nes.png",
+    assetPath: "super_nes",
     name: "Super Nintendo Entertainment System",
     abbreviation: "SNES",
     type: GamePlatformType.stationary,
   ),
   nintendo64(
     family: GamePlatformFamily.nintendo,
-    assetPath: "nintendo_64.png",
+    assetPath: "nintendo_64",
     name: "Nintendo 64",
     abbreviation: "N64",
     type: GamePlatformType.stationary,
   ),
   gameCube(
     family: GamePlatformFamily.nintendo,
-    assetPath: "game_cube.png",
+    assetPath: "game_cube",
     name: "Nintendo GameCube",
     abbreviation: "GCN",
     type: GamePlatformType.stationary,
   ),
   wii(
     family: GamePlatformFamily.nintendo,
-    assetPath: "wii.png",
+    assetPath: "wii",
     name: "Nintendo Wii",
     abbreviation: "Wii",
     type: GamePlatformType.stationary,
   ),
   wiiU(
     family: GamePlatformFamily.nintendo,
-    assetPath: "wii_u.png",
+    assetPath: "wii_u",
     name: "Nintendo Wii U",
     abbreviation: "Wii U",
     type: GamePlatformType.stationary,
   ),
   nintendoSwitch(
     family: GamePlatformFamily.nintendo,
-    assetPath: "nintendo_switch.png",
+    assetPath: "nintendo_switch",
     name: "Nintendo Switch",
     abbreviation: "Switch",
     type: GamePlatformType.stationary,
@@ -135,14 +135,14 @@ enum GamePlatform {
   // ### Handhelds ########################################################## //
   playStationPortable(
     family: GamePlatformFamily.sony,
-    assetPath: "psp.png",
+    assetPath: "psp",
     name: "PlayStation Portable",
     abbreviation: "PSP",
     type: GamePlatformType.handheld,
   ),
   playStationVita(
     family: GamePlatformFamily.sony,
-    assetPath: "ps_vita.png",
+    assetPath: "ps_vita",
     name: "PlayStation Vita",
     abbreviation: "PS Vita",
     type: GamePlatformType.handheld,
@@ -150,35 +150,35 @@ enum GamePlatform {
   // ### Stationaries ####################################################### //
   playStation1(
     family: GamePlatformFamily.sony,
-    assetPath: "ps1.png",
+    assetPath: "ps1",
     name: "PlayStation",
     abbreviation: "PSX",
     type: GamePlatformType.stationary,
   ),
   playStation2(
     family: GamePlatformFamily.sony,
-    assetPath: "ps2.png",
+    assetPath: "ps2",
     name: "PlayStation 2",
     abbreviation: "PS2",
     type: GamePlatformType.stationary,
   ),
   playStation3(
     family: GamePlatformFamily.sony,
-    assetPath: "ps3.png",
+    assetPath: "ps3",
     name: "PlayStation 3",
     abbreviation: "PS3",
     type: GamePlatformType.stationary,
   ),
   playStation4(
     family: GamePlatformFamily.sony,
-    assetPath: "ps4.png",
+    assetPath: "ps4",
     name: "PlayStation 4",
     abbreviation: "PS4",
     type: GamePlatformType.stationary,
   ),
   playStation5(
     family: GamePlatformFamily.sony,
-    assetPath: "ps5.png",
+    assetPath: "ps5",
     name: "PlayStation 5",
     abbreviation: "PS5",
     type: GamePlatformType.stationary,
@@ -187,28 +187,28 @@ enum GamePlatform {
   // ### Stationaries ####################################################### //
   xbox(
     family: GamePlatformFamily.microsoft,
-    assetPath: "xbox.png",
+    assetPath: "xbox",
     name: "XBox",
     abbreviation: "XBox",
     type: GamePlatformType.stationary,
   ),
   xbox360(
     family: GamePlatformFamily.microsoft,
-    assetPath: "xbox_360.png",
+    assetPath: "xbox_360",
     name: "XBox 360",
     abbreviation: "XBox 360",
     type: GamePlatformType.stationary,
   ),
   xboxOne(
     family: GamePlatformFamily.microsoft,
-    assetPath: "xbox_one.png",
+    assetPath: "xbox_one",
     name: "XBox One",
     abbreviation: "XBone",
     type: GamePlatformType.stationary,
   ),
   xboxSeries(
     family: GamePlatformFamily.microsoft,
-    assetPath: "xbox_series.png",
+    assetPath: "xbox_series",
     name: "XBox Series",
     abbreviation: "XBox Series",
     type: GamePlatformType.stationary,
@@ -217,49 +217,49 @@ enum GamePlatform {
   // ### Stationaries ####################################################### //
   pcMisc(
     family: GamePlatformFamily.pc,
-    assetPath: "pc.png",
+    assetPath: "pc",
     name: "PC",
     abbreviation: "PC",
     type: GamePlatformType.stationary,
   ),
   gog(
     family: GamePlatformFamily.pc,
-    assetPath: "gog.png",
+    assetPath: "gog",
     name: "Gog",
     abbreviation: "Gog",
     type: GamePlatformType.stationary,
   ),
   origin(
     family: GamePlatformFamily.pc,
-    assetPath: "origin.png",
+    assetPath: "origin",
     name: "Origin",
     abbreviation: "EA",
     type: GamePlatformType.stationary,
   ),
   steam(
     family: GamePlatformFamily.pc,
-    assetPath: "steam.png",
+    assetPath: "steam",
     name: "Steam",
     abbreviation: "Steam",
     type: GamePlatformType.stationary,
   ),
   ubisoftConnect(
     family: GamePlatformFamily.pc,
-    assetPath: "ubisoft_connect.png",
+    assetPath: "ubisoft_connect",
     name: "Ubisoft Connect",
     abbreviation: "Ubi",
     type: GamePlatformType.stationary,
   ),
   epicGames(
     family: GamePlatformFamily.pc,
-    assetPath: "epic_games.png",
+    assetPath: "epic_games",
     name: "Epic",
     abbreviation: "Epic",
     type: GamePlatformType.stationary,
   ),
   twitch(
     family: GamePlatformFamily.pc,
-    assetPath: "twitch.png",
+    assetPath: "twitch",
     name: "Twitch",
     abbreviation: "Twitch",
     type: GamePlatformType.stationary,
@@ -268,7 +268,7 @@ enum GamePlatform {
   unknown(
     abbreviation: 'unknown',
     family: GamePlatformFamily.misc,
-    assetPath: 'unknown.png',
+    assetPath: 'unknown',
     name: 'unknown',
     type: GamePlatformType.stationary,
   ),
@@ -288,12 +288,13 @@ enum GamePlatform {
     required this.family,
   });
 
-  String get iconPath => "$_iconBasePath/${family.name}/$assetPath";
-  String get textLogoPath => "$_textLogoBasePath/${family.name}/$assetPath";
+  String get iconPath => "$_iconBasePath/${family.name}/$assetPath.webp";
+  String get textLogoPath =>
+      "$_textLogoBasePath/${family.name}/$assetPath.webp";
   String get controllerLogoPathLight =>
-      "$_controllerBasePath/${family.name}/${assetPath.replaceFirst('.png', '_light.png')}";
+      "$_controllerBasePath/${family.name}/${assetPath}_light.webp";
   String get controllerLogoPathDark =>
-      "$_controllerBasePath/${family.name}/${assetPath.replaceFirst('.png', '_dark.png')}";
+      "$_controllerBasePath/${family.name}/${assetPath}_dark.webp";
 
   String controllerLogoPath(BuildContext context) {
     final theme = Theme.of(context);
