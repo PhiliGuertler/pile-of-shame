afe501f6416880123bb18020d4ec5b1981a1682c
Philipp Guertler <phili.gue@hotmail.de>
2023-08-24 18:36:28 +0200

    added a loading animation to games_screen and fading animations

--- lib/features/games/games_list/screens/games_screen.dart
+++ lib/features/games/games_list/screens/games_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pile_of_shame/providers/game_provider.dart';
 import 'package:pile_of_shame/widgets/error_display.dart';
+import 'package:pile_of_shame/widgets/skeletons/skeleton_list_tile.dart';
 
 import '../widgets/game_list_tile.dart';
 
@@ -20,23 +22,38 @@ class GamesScreen extends ConsumerWidget {
             child: CustomScrollView(
               physics: const BouncingScrollPhysics(
                   parent: AlwaysScrollableScrollPhysics()),
-              slivers: games
-                  .map(
-                    (game) => SliverToBoxAdapter(
-                      child: GameListTile(game: game),
-                    ),
-                  )
-                  .toList(),
-            ),
+              slivers: [
+                ...games
+                    .map(
+                      (game) => SliverToBoxAdapter(
+                        child: GameListTile(game: game),
+                      ),
+                    )
+                    .toList(),
+                const SliverToBoxAdapter(
+                  child: SizedBox(
+                    height: 48.0,
+                  ),
+                ),
+              ],
+            ).animate().fadeIn(),
           );
         },
         error: (error, stackTrace) => ErrorDisplay(
           error: error,
           stackTrace: stackTrace,
-        ),
-        loading: () => const Center(
-          child: CircularProgressIndicator(),
-        ),
+        ).animate().fadeIn(),
+        loading: () => CustomScrollView(
+          physics: const NeverScrollableScrollPhysics(),
+          slivers: [
+            for (int i = 0; i < 10; ++i)
+              const SliverToBoxAdapter(
+                child: ListTileSkeleton(
+                  hasSubtitle: true,
+                ),
+              ),
+          ],
+        ).animate().fadeIn(),
       ),
     );
   }
