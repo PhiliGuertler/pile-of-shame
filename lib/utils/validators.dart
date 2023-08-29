a6e23544f365a7378e4ab2359d5f1f33463eab92
Philipp Guertler <phili.gue@hotmail.de>
2023-08-29 13:49:51 +0200

    added a class for validators of input fields

---
+++ lib/utils/validators.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:pile_of_shame/l10n/generated/app_localizations.dart';
+
+class Validators {
+  const Validators._();
+
+  static String? Function(String? value) validateFieldIsRequired(
+          BuildContext context) =>
+      (String? value) {
+        if (value == null || value.isEmpty) {
+          return AppLocalizations.of(context)!.thisFieldIsRequired;
+        }
+        return null;
+      };
+}
