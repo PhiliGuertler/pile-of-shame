d2f69f741a3536fdfcaeb6e81000698d3324feed
Philipp Guertler <phili.gue@hotmail.de>
2023-08-31 18:32:35 +0200

    added a few providers for filtering

---
+++ lib/providers/games/game_filter_provider.dart
@@ -0,0 +1,77 @@
+import 'package:pile_of_shame/models/age_restriction.dart';
+import 'package:pile_of_shame/models/game.dart';
+import 'package:pile_of_shame/models/game_platforms.dart';
+import 'package:pile_of_shame/models/play_status.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'game_filter_provider.g.dart';
+
+@riverpod
+class PlayStatusFilter extends _$PlayStatusFilter {
+  @override
+  List<PlayStatus> build() {
+    return PlayStatus.values;
+  }
+
+  void setFilter(List<PlayStatus> filter) {
+    state = filter;
+  }
+}
+
+@riverpod
+class GamePlatformFamilyFilter extends _$GamePlatformFamilyFilter {
+  @override
+  List<GamePlatformFamily> build() {
+    return GamePlatformFamily.values;
+  }
+
+  void setFilter(List<GamePlatformFamily> filter) {
+    state = filter;
+  }
+}
+
+@riverpod
+class GamePlatformFilter extends _$GamePlatformFilter {
+  @override
+  List<GamePlatform> build() {
+    return GamePlatform.values;
+  }
+
+  void setFilter(List<GamePlatform> filter) {
+    state = filter;
+  }
+}
+
+@riverpod
+class AgeRatingFilter extends _$AgeRatingFilter {
+  @override
+  List<USK> build() {
+    return USK.values;
+  }
+
+  void setFilter(List<USK> filter) {
+    state = filter;
+  }
+}
+
+@riverpod
+List<Game> filteredGames(FilteredGamesRef ref, List<Game> games) {
+  final platformFilter = ref.watch(gamePlatformFilterProvider);
+  final platformFamilyFilter = ref.watch(gamePlatformFamilyFilterProvider);
+  final playStatusFilter = ref.watch(playStatusFilterProvider);
+  final ageRatingFilter = ref.watch(ageRatingFilterProvider);
+
+  final result = games.where((Game game) {
+    bool isContainedInPlatform = platformFilter.contains(game.platform);
+    bool isContainedInPlatformFamily =
+        platformFamilyFilter.contains(game.platform.family);
+    bool isContainedInPlayStatus = playStatusFilter.contains(game.status);
+    bool isContainedInAgeRatings = ageRatingFilter.contains(game.usk);
+    return isContainedInPlatform &&
+        isContainedInPlatformFamily &&
+        isContainedInPlayStatus &&
+        isContainedInAgeRatings;
+  }).toList();
+
+  return result;
+}
