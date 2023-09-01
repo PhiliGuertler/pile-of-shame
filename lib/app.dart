33dc57668534c44da9852f075856099dac2ed411
Philipp Guertler <phili.gue@hotmail.de>
2023-09-01 12:40:35 +0200

    added automatic snackbars on uncaught errors

--- lib/app.dart
+++ lib/app.dart
@@ -38,6 +38,7 @@ class App extends ConsumerWidget {
       final ColorScheme darkColorScheme = appTheme.computeColorScheme(false);
 
       return MaterialApp(
+        scaffoldMessengerKey: scaffoldMessengerKey,
         theme: _createThemeData(
           lightColorScheme,
         ),
--- lib/initializer.dart
+++ lib/initializer.dart
@@ -1,7 +1,10 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pile_of_shame/app.dart';
+import 'package:pile_of_shame/providers/debug_provider.dart';
 
 class Initializer {
   void setupInitialization() {
@@ -9,11 +12,38 @@ class Initializer {
     FlutterNativeSplash.preserve(widgetsBinding: binding);
   }
 
-  Future<void> initializeApp() async {
-    // TODO: Do some async initialization here
-    if (kDebugMode) {
+  void _setupAutomaticErrorDisplay(ProviderContainer container) {
+    // as there is no Buildcontext yet, we cannot access translations here.
+    const String unknownErrorMessage = "Unknown Error";
+
+    final originalError = FlutterError.onError;
+    FlutterError.onError = (details) {
+      final isDebugMode = container.read(debugFeatureAccessProvider);
+
+      SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
+        scaffoldMessengerKey.currentState?.showSnackBar(
+          SnackBar(
+            backgroundColor: Colors.red.shade800,
+            content: Text(
+              isDebugMode
+                  ? details.summary.toDescription()
+                  : unknownErrorMessage,
+              style: const TextStyle(color: Colors.white),
+            ),
+          ),
+        );
+      });
+      originalError?.call(details);
+    };
+  }
+
+  Future<void> initializeApp(ProviderContainer container) async {
+    final isDebugMode = container.read(debugFeatureAccessProvider);
+    if (isDebugMode) {
       Animate.restartOnHotReload = true;
     }
+
+    _setupAutomaticErrorDisplay(container);
 
     FlutterNativeSplash.remove();
   }
