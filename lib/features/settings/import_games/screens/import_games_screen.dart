fec3308ec99d99c4521a92bfac631aff3f01e394
Philipp Guertler <phili.gue@hotmail.de>
2023-09-01 14:02:23 +0200

    added a settings screen for importing files

---
+++ lib/features/settings/import_games/screens/import_games_screen.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pile_of_shame/l10n/generated/app_localizations.dart';
+import 'package:pile_of_shame/providers/file_provider.dart';
+import 'package:pile_of_shame/providers/games/game_provider.dart';
+import 'package:pile_of_shame/utils/constants.dart';
+import 'package:pile_of_shame/widgets/segmented_action_card.dart';
+
+class ImportGamesScreen extends ConsumerStatefulWidget {
+  const ImportGamesScreen({super.key});
+
+  @override
+  ConsumerState<ImportGamesScreen> createState() => _ImportGamesScreenState();
+}
+
+class _ImportGamesScreenState extends ConsumerState<ImportGamesScreen> {
+  bool isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final trailing = isLoading
+        ? const SizedBox(
+            width: 28, height: 28, child: CircularProgressIndicator())
+        : const Icon(Icons.file_download);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(AppLocalizations.of(context)!.importGames),
+      ),
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.only(
+                    left: defaultPaddingX,
+                    right: defaultPaddingX,
+                    top: 24.0,
+                    bottom: 16.0),
+                child: Text(AppLocalizations.of(context)!
+                    .beforeYouImportGamesYouShouldExportYourPreviousGamesAsAPrecaution),
+              ),
+            ),
+            SliverToBoxAdapter(
+              child: SegmentedActionCard(
+                items: [
+                  SegmentedActionCardItem(
+                    trailing: trailing,
+                    title:
+                        Text(AppLocalizations.of(context)!.importAndOverride),
+                    subtitle: Text(AppLocalizations.of(context)!
+                        .yourPreviousGamesWillBeDeleted),
+                    onTap: isLoading
+                        ? null
+                        : () async {
+                            setState(() {
+                              isLoading = true;
+                            });
+                            final pickedFile =
+                                await ref.read(fileUtilsProvider).pickFile();
+                            if (pickedFile != null) {
+                              try {
+                                final gameStorage =
+                                    ref.read(gameStorageProvider);
+                                final games = await gameStorage
+                                    .readGamesFromFile(pickedFile);
+                                await gameStorage.persistGamesList(games);
+
+                                if (context.mounted) {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                      content: Text(
+                                          AppLocalizations.of(context)!
+                                              .importSuccessful),
+                                    ),
+                                  );
+                                }
+                              } catch (error) {
+                                setState(() {
+                                  isLoading = false;
+                                });
+                                if (context.mounted) {
+                                  throw Exception(AppLocalizations.of(context)!
+                                      .importFailed);
+                                }
+                              }
+                            } else {
+                              if (context.mounted) {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                    content: Text(AppLocalizations.of(context)!
+                                        .importCancelled),
+                                  ),
+                                );
+                              }
+                            }
+                            setState(() {
+                              isLoading = false;
+                            });
+                          },
+                  ),
+                  SegmentedActionCardItem(
+                    trailing: trailing,
+                    title: Text(AppLocalizations.of(context)!.importAndUpdate),
+                    subtitle: Text(
+                        "${AppLocalizations.of(context)!.gamesInYourListThatAreOlderThanImportedGamesWillBeOverwritten} ${AppLocalizations.of(context)!.gamesThatAreNotInYourListWillBeImported}"),
+                    onTap: () async {
+                      throw UnimplementedError();
+                    },
+                  ),
+                  SegmentedActionCardItem(
+                    trailing: trailing,
+                    title:
+                        Text(AppLocalizations.of(context)!.updateExistingGames),
+                    subtitle: Text(AppLocalizations.of(context)!
+                        .gamesInYourListThatAreOlderThanImportedGamesWillBeOverwritten),
+                    onTap: () async {
+                      throw UnimplementedError();
+                    },
+                  ),
+                  SegmentedActionCardItem(
+                    trailing: trailing,
+                    title: Text(
+                        AppLocalizations.of(context)!.importMissingGamesOnly),
+                    subtitle: Text(AppLocalizations.of(context)!
+                        .gamesThatAreNotInYourListWillBeImported),
+                    onTap: () async {
+                      throw UnimplementedError();
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/features/settings/root/screens/settings_screen.dart
+++ lib/features/settings/root/screens/settings_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:pile_of_shame/features/settings/appearance/screens/appearance_screen.dart';
+import 'package:pile_of_shame/features/settings/import_games/screens/import_games_screen.dart';
 import 'package:pile_of_shame/features/settings/language/screens/language_screen.dart';
 import 'package:pile_of_shame/l10n/generated/app_localizations.dart';
 import 'package:pile_of_shame/providers/debug_provider.dart';
@@ -80,8 +81,9 @@ class SettingsScreen extends ConsumerWidget {
                   subtitle: Text(
                       AppLocalizations.of(context)!.importGamesFromAJSONFile),
                   onTap: () {
-                    // TODO: Implement button
-                    throw UnimplementedError();
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const ImportGamesScreen(),
+                    ));
                   },
                 ),
                 SegmentedActionCardItem(
