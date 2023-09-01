d8fdfad2af7b29be44d3fcce74c2c4e6d7d24841
Philipp Guertler <phili.gue@hotmail.de>
2023-09-01 11:21:31 +0200

    updated games screen to be a bit smoother

--- lib/features/games/games_list/screens/games_screen.dart
+++ lib/features/games/games_list/screens/games_screen.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pile_of_shame/features/games/games_list/widgets/sliver_filtered_games.dart';
 import 'package:pile_of_shame/providers/games/game_provider.dart';
 import 'package:pile_of_shame/widgets/error_display.dart';
 import 'package:pile_of_shame/widgets/skeletons/skeleton_game_display.dart';
 
-import '../widgets/game_list_tile.dart';
+import '../widgets/sliver_filter_by_family.dart';
 
 class GamesScreen extends ConsumerWidget {
   final ScrollController scrollController;
@@ -14,44 +15,40 @@ class GamesScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final games = ref.watch(gamesProvider);
+    final hasGames = ref.watch(hasGamesProvider);
 
     return SafeArea(
-      child: games.when(
-        data: (games) {
-          return RefreshIndicator(
-            onRefresh: () => ref.refresh(gamesProvider.future),
-            child: CustomScrollView(
-              controller: scrollController,
-              physics: const BouncingScrollPhysics(
-                  parent: AlwaysScrollableScrollPhysics()),
-              slivers: [
-                SliverList.builder(
-                  itemBuilder: (context, index) =>
-                      GameListTile(game: games.games[index]),
-                  itemCount: games.games.length,
+      child: hasGames.when(
+        data: (hasGames) => RefreshIndicator(
+          onRefresh: () => ref.refresh(gamesProvider.future),
+          child: CustomScrollView(
+            controller: scrollController,
+            physics: const BouncingScrollPhysics(
+                parent: AlwaysScrollableScrollPhysics()),
+            slivers: const [
+              SliverContractSorterFilter(),
+              SliverFilteredGames(),
+              SliverToBoxAdapter(
+                child: SizedBox(
+                  height: 48.0,
                 ),
-                const SliverToBoxAdapter(
-                  child: SizedBox(
-                    height: 48.0,
-                  ),
-                ),
-              ],
-            ).animate().fadeIn(),
-          );
-        },
-        error: (error, stackTrace) => ErrorDisplay(
-          error: error,
-          stackTrace: stackTrace,
-        ).animate().fadeIn(),
+              ),
+            ],
+          ),
+        ),
         loading: () => CustomScrollView(
           physics: const NeverScrollableScrollPhysics(),
           slivers: [
+            const SliverContractSorterFilterSkeleton(),
             SliverList.builder(
               itemBuilder: (context, index) => const SkeletonGameDisplay(),
               itemCount: 10,
-            ),
+            )
           ],
+        ).animate().fadeIn(),
+        error: (error, stackTrace) => ErrorDisplay(
+          error: error,
+          stackTrace: stackTrace,
         ).animate().fadeIn(),
       ),
     );
