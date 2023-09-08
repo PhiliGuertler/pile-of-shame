1eca348765079bd6a3f256e7bbf244beec5c5a8b
Philipp Guertler <phili.gue@hotmail.de>
2023-09-08 23:59:06 +0200

    added a wrapper around the app that checks for shared files

---
+++ lib/providers/shared_content_provider.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_sharing_intent/model/sharing_file.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'shared_content_provider.g.dart';
+
+@riverpod
+class SharedContent extends _$SharedContent {
+  @override
+  List<SharedFile> build() {
+    return [];
+  }
+
+  void setFiles(List<SharedFile> files) {
+    state = files;
+  }
+}
+
+@riverpod
+bool hasSharedContent(HasSharedContentRef ref) {
+  final content = ref.watch(sharedContentProvider);
+  return content.isNotEmpty;
+}
