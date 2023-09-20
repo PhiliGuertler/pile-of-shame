abc02187e748d92c888504158c000f4d0063705d
Philipp Guertler <phili.gue@hotmail.de>
2023-09-20 20:00:24 +0200

    added a small text that explains what pile of shame actually is

--- lib/features/settings/about/screens/about_screen.dart
+++ lib/features/settings/about/screens/about_screen.dart
@@ -15,55 +15,68 @@ class AboutScreen extends StatelessWidget {
     return AppScaffold(
       appBar: AppBar(title: Text(AppLocalizations.of(context)!.aboutThisApp)),
       body: SafeArea(
-        child: SegmentedActionCard(
-          items: [
-            SegmentedActionCardItem(
-              title: Text(AppLocalizations.of(context)!.githubRepository),
-              subtitle:
-                  const Text("https://github.com/PhiliGuertler/pile-of-shame"),
-              trailing: const Icon(Icons.open_in_new),
-              onTap: () async {
-                if (!await launchUrl(
-                  Uri.parse("https://github.com/PhiliGuertler/pile-of-shame"),
-                )) {
-                  throw Exception("Could not launch url");
-                }
-              },
+        child: ListView(
+          children: [
+            SegmentedActionCard(
+              items: [
+                SegmentedActionCardItem(
+                  title: Text(AppLocalizations.of(context)!.githubRepository),
+                  subtitle: const Text(
+                      "https://github.com/PhiliGuertler/pile-of-shame"),
+                  trailing: const Icon(Icons.open_in_new),
+                  onTap: () async {
+                    if (!await launchUrl(
+                      Uri.parse(
+                          "https://github.com/PhiliGuertler/pile-of-shame"),
+                    )) {
+                      throw Exception("Could not launch url");
+                    }
+                  },
+                ),
+                SegmentedActionCardItem(
+                  title: Text(AppLocalizations.of(context)!.licenses),
+                  trailing: const Icon(Icons.library_books),
+                  onTap: () async {
+                    final info = await PackageInfo.fromPlatform();
+                    if (context.mounted) {
+                      showAboutDialog(
+                        context: context,
+                        applicationIcon: ImageContainer(
+                            child: Image.asset('assets/app/logo.png')),
+                        applicationName: info.appName,
+                        applicationVersion:
+                            "${info.version} (${info.buildNumber})",
+                        children: [
+                          ClipRRect(
+                            borderRadius:
+                                BorderRadius.circular(defaultBorderRadius),
+                            child: SizedBox(
+                              height: 200,
+                              child: Image.asset(
+                                'assets/misc/loading.webp',
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          )
+                        ],
+                      );
+                    }
+                  },
+                ),
+                SegmentedActionCardItem(
+                  title: Text(AppLocalizations.of(context)!.imageCredit),
+                  subtitle: Text(AppLocalizations.of(context)!
+                      .imagesOfControllersHaveBeenCreatedUsingMidjourney),
+                  trailing: const SizedBox(),
+                ),
+              ],
             ),
-            SegmentedActionCardItem(
-              title: Text(AppLocalizations.of(context)!.licenses),
-              trailing: const Icon(Icons.library_books),
-              onTap: () async {
-                final info = await PackageInfo.fromPlatform();
-                if (context.mounted) {
-                  showAboutDialog(
-                    context: context,
-                    applicationIcon: ImageContainer(
-                        child: Image.asset('assets/app/logo.png')),
-                    applicationName: info.appName,
-                    applicationVersion: "${info.version} (${info.buildNumber})",
-                    children: [
-                      ClipRRect(
-                        borderRadius:
-                            BorderRadius.circular(defaultBorderRadius),
-                        child: SizedBox(
-                          height: 200,
-                          child: Image.asset(
-                            'assets/misc/loading.webp',
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      )
-                    ],
-                  );
-                }
-              },
-            ),
-            SegmentedActionCardItem(
-              title: Text(AppLocalizations.of(context)!.imageCredit),
-              subtitle: Text(AppLocalizations.of(context)!
-                  .imagesOfControllersHaveBeenCreatedUsingMidjourney),
-              trailing: const SizedBox(),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: defaultPaddingX,
+                vertical: 16.0,
+              ),
+              child: Text(AppLocalizations.of(context)!.pileOfShameWhatAreYou),
             ),
           ],
         ),
