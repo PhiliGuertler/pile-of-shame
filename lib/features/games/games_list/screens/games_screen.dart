6b8444cad2897de09001e432aa981a4034c4672a
Philipp Guertler <phili.gue@hotmail.de>
2023-09-20 17:19:29 +0200

    added auto close behavior of slidables in games screen

--- lib/features/games/games_list/screens/games_screen.dart
+++ lib/features/games/games_list/screens/games_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:pile_of_shame/features/games/games_list/widgets/slivers/sliver_grouped_games.dart';
 import 'package:pile_of_shame/l10n/generated/app_localizations.dart';
 import 'package:pile_of_shame/providers/format_provider.dart';
@@ -32,107 +33,109 @@ class GamesScreen extends ConsumerWidget {
         skipLoadingOnReload: true,
         data: (hasGames) => RefreshIndicator(
           onRefresh: () => ref.refresh(gamesProvider.future),
-          child: CustomScrollView(
-            keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-            controller: scrollController,
-            physics: !hasGames
-                ? const BouncingScrollPhysics(
-                    parent: AlwaysScrollableScrollPhysics(),
-                  )
-                : null,
-            slivers: [
-              if (!hasGames)
-                const SliverFancyImageHeader(
-                    imagePath: 'assets/misc/game_pile.webp', height: 300.0),
-              if (!hasGames)
-                SliverToBoxAdapter(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: defaultPaddingX, vertical: 16.0),
-                    child: Text(
-                      AppLocalizations.of(context)!
-                          .buildYourPileOfShameByAddingNewGames,
-                      style: Theme.of(context).textTheme.headlineSmall,
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                ),
-              if (hasGames) const SliverContractSorterFilter(),
-              if (hasGames)
-                ...groupedGames.when(
-                  skipLoadingOnReload: true,
-                  data: (groupedGames) {
-                    return groupedGames.entries
-                        .map((group) => SliverGroupedGames(
-                              games: group.value,
-                              groupName: group.key,
-                            ));
-                  },
-                  error: (error, stackTrace) => [
-                    SliverToBoxAdapter(
-                      child: ErrorDisplay(
-                        error: error,
-                        stackTrace: stackTrace,
+          child: SlidableAutoCloseBehavior(
+            child: CustomScrollView(
+              keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+              controller: scrollController,
+              physics: !hasGames
+                  ? const BouncingScrollPhysics(
+                      parent: AlwaysScrollableScrollPhysics(),
+                    )
+                  : null,
+              slivers: [
+                if (!hasGames)
+                  const SliverFancyImageHeader(
+                      imagePath: 'assets/misc/game_pile.webp', height: 300.0),
+                if (!hasGames)
+                  SliverToBoxAdapter(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: defaultPaddingX, vertical: 16.0),
+                      child: Text(
+                        AppLocalizations.of(context)!
+                            .buildYourPileOfShameByAddingNewGames,
+                        style: Theme.of(context).textTheme.headlineSmall,
+                        textAlign: TextAlign.center,
                       ),
                     ),
-                  ],
-                  loading: () => [
-                    SliverList.builder(
-                      itemBuilder: (context, index) =>
-                          const SkeletonGameDisplay()
-                              .animate()
-                              .fade(curve: Curves.easeOut, duration: 130.ms),
-                      itemCount: 10,
-                    ),
-                  ],
-                ),
-              if (hasGames)
-                SliverToBoxAdapter(
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                      top: 16.0,
-                      left: defaultPaddingX,
-                      right: defaultPaddingX,
-                      bottom: 80.0,
-                    ),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        SizedBox(
-                          width: 128,
-                          child: totalGames.when(
-                            data: (totalGames) => Text(
-                                AppLocalizations.of(context)!
-                                    .nGames(totalGames)),
-                            loading: () => const Skeleton(
-                              widthFactor: 1,
-                            ),
-                            error: (error, stackTrace) =>
-                                Text(error.toString()),
-                          ),
+                  ),
+                if (hasGames) const SliverContractSorterFilter(),
+                if (hasGames)
+                  ...groupedGames.when(
+                    skipLoadingOnReload: true,
+                    data: (groupedGames) {
+                      return groupedGames.entries
+                          .map((group) => SliverGroupedGames(
+                                games: group.value,
+                                groupName: group.key,
+                              ));
+                    },
+                    error: (error, stackTrace) => [
+                      SliverToBoxAdapter(
+                        child: ErrorDisplay(
+                          error: error,
+                          stackTrace: stackTrace,
                         ),
-                        SizedBox(
-                          width: 128,
-                          child: Align(
-                            alignment: Alignment.centerRight,
-                            child: totalPrice.when(
-                              data: (totalPrice) =>
-                                  Text(currencyFormatter.format(totalPrice)),
+                      ),
+                    ],
+                    loading: () => [
+                      SliverList.builder(
+                        itemBuilder: (context, index) =>
+                            const SkeletonGameDisplay()
+                                .animate()
+                                .fade(curve: Curves.easeOut, duration: 130.ms),
+                        itemCount: 10,
+                      ),
+                    ],
+                  ),
+                if (hasGames)
+                  SliverToBoxAdapter(
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        top: 16.0,
+                        left: defaultPaddingX,
+                        right: defaultPaddingX,
+                        bottom: 80.0,
+                      ),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          SizedBox(
+                            width: 128,
+                            child: totalGames.when(
+                              data: (totalGames) => Text(
+                                  AppLocalizations.of(context)!
+                                      .nGames(totalGames)),
                               loading: () => const Skeleton(
                                 widthFactor: 1,
-                                alignment: Alignment.centerRight,
                               ),
                               error: (error, stackTrace) =>
                                   Text(error.toString()),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ).animate().fadeIn(),
-                ),
-            ],
+                          SizedBox(
+                            width: 128,
+                            child: Align(
+                              alignment: Alignment.centerRight,
+                              child: totalPrice.when(
+                                data: (totalPrice) =>
+                                    Text(currencyFormatter.format(totalPrice)),
+                                loading: () => const Skeleton(
+                                  widthFactor: 1,
+                                  alignment: Alignment.centerRight,
+                                ),
+                                error: (error, stackTrace) =>
+                                    Text(error.toString()),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ).animate().fadeIn(),
+                  ),
+              ],
+            ),
           ),
         ),
         loading: () => CustomScrollView(
