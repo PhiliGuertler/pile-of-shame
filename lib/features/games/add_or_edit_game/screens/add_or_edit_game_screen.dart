351abb490fffb47958f6ebc96c2fe15f6526ce49
Philipp Guertler <phili.gue@hotmail.de>
2023-09-20 15:01:30 +0200

    moved platform and usk dropdowns to their own widgets

--- lib/features/games/add_or_edit_game/screens/add_or_edit_game_screen.dart
+++ lib/features/games/add_or_edit_game/screens/add_or_edit_game_screen.dart
@@ -1,22 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pile_of_shame/features/games/add_or_edit_game/providers/edit_game_provider.dart';
-import 'package:pile_of_shame/features/games/add_or_edit_game/widgets/game_platform_input_field.dart';
 import 'package:pile_of_shame/features/games/add_or_edit_game/widgets/name_input_field.dart';
 import 'package:pile_of_shame/features/games/add_or_edit_game/widgets/notes_input_field.dart';
+import 'package:pile_of_shame/features/games/add_or_edit_game/widgets/platform_dropdown.dart';
 import 'package:pile_of_shame/features/games/add_or_edit_game/widgets/price_input_field.dart';
+import 'package:pile_of_shame/features/games/add_or_edit_game/widgets/usk_dropdown.dart';
 import 'package:pile_of_shame/l10n/generated/app_localizations.dart';
-import 'package:pile_of_shame/models/age_restriction.dart';
 import 'package:pile_of_shame/models/game.dart';
-import 'package:pile_of_shame/models/game_platforms.dart';
-import 'package:pile_of_shame/providers/games/game_platforms_provider.dart';
 import 'package:pile_of_shame/utils/constants.dart';
-import 'package:pile_of_shame/utils/validators.dart';
 import 'package:pile_of_shame/widgets/app_scaffold.dart';
-import 'package:pile_of_shame/widgets/game_platform_icon.dart';
-import 'package:pile_of_shame/widgets/input/dropdown_search_field.dart';
 import 'package:pile_of_shame/widgets/segmented_action_card.dart';
-import 'package:pile_of_shame/widgets/usk_logo.dart';
 
 import '../models/editable_game.dart';
 import '../widgets/play_status_dropdown.dart';
@@ -37,22 +31,6 @@ class _AddGameScreenState extends ConsumerState<AddGameScreen> {
   @override
   Widget build(BuildContext context) {
     final editableGame = ref.watch(addGameProvider(widget.initialValue));
-
-    final gamePlatforms = ref.watch(activeGamePlatformsProvider);
-    final allPlatforms = gamePlatforms.maybeWhen(
-      data: (data) => data,
-      orElse: () => GamePlatform.values,
-    );
-
-    final List<GamePlatform> sortedGamePlatforms = List.from(allPlatforms);
-    sortedGamePlatforms.sort(
-      (a, b) => a
-          .localizedName(AppLocalizations.of(context)!)
-          .toLowerCase()
-          .compareTo(
-            b.localizedName(AppLocalizations.of(context)!).toLowerCase(),
-          ),
-    );
 
     return AppScaffold(
       appBar: AppBar(
@@ -81,44 +59,8 @@ class _AddGameScreenState extends ConsumerState<AddGameScreen> {
                 Padding(
                   padding:
                       const EdgeInsets.symmetric(horizontal: defaultPaddingX),
-                  child: DropdownSearchField<GamePlatform>(
-                    key: const ValueKey("game_platform_dropdown"),
+                  child: PlatformDropdown(
                     value: editableGame.platform,
-                    filter: (searchTerm, option) {
-                      // check if the platform family is matching
-                      final matchesFamily =
-                          option.family.name.toLowerCase().contains(searchTerm);
-
-                      // check if the platform's abbreviation contains all the search terms
-                      final matchesAbbreviation = option.abbreviation
-                          .toLowerCase()
-                          .contains(searchTerm);
-                      // check if the platform's name conains all the search terms
-                      final matchesName = option
-                          .localizedName(AppLocalizations.of(context)!)
-                          .toLowerCase()
-                          .contains(searchTerm);
-
-                      return matchesFamily ||
-                          matchesAbbreviation ||
-                          matchesName;
-                    },
-                    optionBuilder: (context, option, onTap) => ListTile(
-                      key: ValueKey(option.abbreviation),
-                      leading: GamePlatformIcon(platform: option),
-                      title: Text(
-                          option.localizedName(AppLocalizations.of(context)!)),
-                      onTap: onTap,
-                    ),
-                    valueBuilder: (context, option, onTap) =>
-                        GamePlatformInputField(
-                      key: const ValueKey("game_platform_input"),
-                      value: option,
-                      label: Text("${AppLocalizations.of(context)!.platform}*"),
-                      onTap: onTap,
-                      validator: Validators.validateFieldIsRequired(context),
-                    ),
-                    options: sortedGamePlatforms,
                     onChanged: (value) {
                       ref
                           .read(addGameProvider(widget.initialValue).notifier)
@@ -153,51 +95,13 @@ class _AddGameScreenState extends ConsumerState<AddGameScreen> {
                 Padding(
                   padding:
                       const EdgeInsets.symmetric(horizontal: defaultPaddingX),
-                  child: DropdownButtonFormField<USK>(
-                    key: const ValueKey("age_rating"),
-                    decoration: InputDecoration(
-                      label: Text(AppLocalizations.of(context)!.ageRating),
-                      prefixIcon: Padding(
-                        padding: const EdgeInsets.only(right: 8.0),
-                        child: USKLogo(ageRestriction: editableGame.usk),
-                      ),
-                      suffixIcon: const Icon(Icons.expand_more),
-                    ),
+                  child: USKDropdown(
                     onChanged: (value) {
-                      if (value != null) {
-                        ref
-                            .read(addGameProvider(widget.initialValue).notifier)
-                            .updateGame(editableGame.copyWith(usk: value));
-                      }
+                      ref
+                          .read(addGameProvider(widget.initialValue).notifier)
+                          .updateGame(editableGame.copyWith(usk: value));
                     },
-                    // Display the text of selected items only, as the prefix-icon takes care of the logo
-                    selectedItemBuilder: (context) => USK.values
-                        .map((usk) => Text(
-                            usk.toRatedString(AppLocalizations.of(context)!)))
-                        .toList(),
-                    // Don't display the default icon, instead display nothing
-                    icon: const SizedBox(),
                     value: editableGame.usk,
-                    items: USK.values
-                        .map(
-                          (usk) => DropdownMenuItem<USK>(
-                            value: usk,
-                            child: Row(
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.only(right: 8.0),
-                                  child: USKLogo(ageRestriction: usk),
-                                ),
-                                Text(
-                                  usk.toRatedString(
-                                      AppLocalizations.of(context)!),
-                                  key: ValueKey(usk.toString()),
-                                ),
-                              ],
-                            ),
-                          ),
-                        )
-                        .toList(),
                   ),
                 ),
                 Padding(
---
+++ lib/features/games/add_or_edit_game/widgets/usk_dropdown.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:pile_of_shame/l10n/generated/app_localizations.dart';
+import 'package:pile_of_shame/models/age_restriction.dart';
+import 'package:pile_of_shame/widgets/usk_logo.dart';
+
+class USKDropdown extends StatelessWidget {
+  final USK value;
+  final void Function(USK value) onChanged;
+
+  const USKDropdown({super.key, required this.value, required this.onChanged});
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButtonFormField<USK>(
+      key: const ValueKey("age_rating"),
+      decoration: InputDecoration(
+        label: Text(AppLocalizations.of(context)!.ageRating),
+        prefixIcon: Padding(
+          padding: const EdgeInsets.only(right: 8.0),
+          child: USKLogo(ageRestriction: value),
+        ),
+        suffixIcon: const Icon(Icons.expand_more),
+      ),
+      onChanged: (value) {
+        if (value != null) {
+          onChanged(value);
+        }
+      },
+      // Display the text of selected items only, as the prefix-icon takes care of the logo
+      selectedItemBuilder: (context) => USK.values
+          .map((usk) => Text(usk.toRatedString(AppLocalizations.of(context)!)))
+          .toList(),
+      // Don't display the default icon, instead display nothing
+      icon: const SizedBox(),
+      value: value,
+      items: USK.values
+          .map(
+            (usk) => DropdownMenuItem<USK>(
+              value: usk,
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 8.0),
+                    child: USKLogo(ageRestriction: usk),
+                  ),
+                  Text(
+                    usk.toRatedString(AppLocalizations.of(context)!),
+                    key: ValueKey(usk.toString()),
+                  ),
+                ],
+              ),
+            ),
+          )
+          .toList(),
+    );
+  }
+}
