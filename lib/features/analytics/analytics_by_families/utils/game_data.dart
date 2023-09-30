0b37911a5dc6bc3a9edc37d8fba0aea58a44067d
Philipp Guertler <phili.gue@hotmail.de>
2023-09-30 16:13:01 +0200

    added a few analyses to game_data

--- lib/features/analytics/analytics_by_families/utils/game_data.dart
+++ lib/features/analytics/analytics_by_families/utils/game_data.dart
@@ -1,3 +1,4 @@
+import 'package:pile_of_shame/features/analytics/analytics_by_families/models/price_span.dart';
 import 'package:pile_of_shame/l10n/generated/app_localizations.dart';
 import 'package:pile_of_shame/models/chart_data.dart';
 import 'package:pile_of_shame/models/game.dart';
@@ -99,5 +100,151 @@ class GameData {
     result.removeWhere((element) => element.value < 0.01);
 
     return result;
+  }
+
+  List<ChartData> toPriceAccumulation() {
+    final List<double> prices = games.map((e) => e.fullPrice()).toList();
+    prices.sort((a, b) => a.compareTo(b));
+
+    final List<ChartData> result = [];
+    for (int i = 0; i < prices.length; ++i) {
+      final price = prices[i];
+      result.add(
+        ChartData(title: "", value: (i + 1).toDouble(), secondaryValue: price),
+      );
+    }
+    return result;
+  }
+
+  List<ChartData> toPriceDistribution(double interval) {
+    assert(interval > 0.0);
+
+    final List<Pair<double, int>> priceDistribution = [];
+
+    final int numGifts = games.fold(
+      0,
+      (previousValue, element) =>
+          element.wasGifted ? previousValue + 1 : previousValue,
+    );
+    priceDistribution.add(Pair(double.negativeInfinity, numGifts));
+    final int totalGames = games.length;
+    int processedGames = numGifts;
+    final int numFreeGames = games.fold(
+      0,
+      (previousValue, element) => !element.wasGifted && element.price < 0.01
+          ? previousValue + 1
+          : previousValue,
+    );
+    priceDistribution.add(Pair(0, numGifts));
+    processedGames += numFreeGames;
+
+    double priceCap = interval;
+
+    while (processedGames < totalGames) {
+      int matchingGames = games.fold(
+        0,
+        (previousValue, element) =>
+            element.fullPrice() < priceCap ? previousValue + 1 : previousValue,
+      );
+      matchingGames -= processedGames;
+      priceDistribution.add(Pair(priceCap, matchingGames));
+
+      processedGames += matchingGames;
+      priceCap += interval;
+    }
+
+    return priceDistribution
+        .map(
+          (e) => ChartData(
+            title: e.first == double.negativeInfinity ? "Gift" : "< ${e.first}",
+            value: e.second.toDouble(),
+            isSelected: highlight ==
+                (e.first == double.negativeInfinity ? "Gift" : "< ${e.first}"),
+          ),
+        )
+        .toList();
+  }
+
+  PriceSpan toPriceSpanWithGifts() {
+    final min = games.fold(
+      double.infinity,
+      (previousValue, element) => element.fullPrice() < previousValue
+          ? element.fullPrice()
+          : previousValue,
+    );
+    final max = games.fold(
+      double.negativeInfinity,
+      (previousValue, element) => element.fullPrice() > previousValue
+          ? element.fullPrice()
+          : previousValue,
+    );
+    final sum = games.fold(
+      0.0,
+      (previousValue, element) => element.fullPrice() + previousValue,
+    );
+
+    return PriceSpan(min: min, max: max, avg: sum / games.length);
+  }
+
+  PriceSpan toPriceSpanWithoutGifts() {
+    final nonGiftedGames = games.where((element) => !element.wasGifted);
+
+    final min = nonGiftedGames.fold(
+      double.infinity,
+      (previousValue, element) => element.fullPrice() < previousValue
+          ? element.fullPrice()
+          : previousValue,
+    );
+    final max = nonGiftedGames.fold(
+      double.negativeInfinity,
+      (previousValue, element) => element.fullPrice() > previousValue
+          ? element.fullPrice()
+          : previousValue,
+    );
+    final sum = nonGiftedGames.fold(
+      0.0,
+      (previousValue, element) => element.fullPrice() + previousValue,
+    );
+
+    return PriceSpan(min: min, max: max, avg: sum / nonGiftedGames.length);
+  }
+
+  PriceSpan toPriceMedianSpanWithGifts() {
+    final prices = games.map((e) => e.fullPrice()).toList();
+    prices.sort((a, b) => a.compareTo(b));
+
+    final min = prices.fold(
+      double.infinity,
+      (previousValue, element) =>
+          element < previousValue ? element : previousValue,
+    );
+    final max = prices.fold(
+      double.negativeInfinity,
+      (previousValue, element) =>
+          element > previousValue ? element : previousValue,
+    );
+    final median = prices[prices.length ~/ 2];
+
+    return PriceSpan(min: min, max: max, avg: median);
+  }
+
+  PriceSpan toPriceMedianSpanWithoutGifts() {
+    final nonGiftedGames = games.where((element) => !element.wasGifted);
+    final prices = nonGiftedGames.map((e) => e.fullPrice()).toList();
+    prices.sort((a, b) => a.compareTo(b));
+
+    final min = prices.fold(
+      double.infinity,
+      (previousValue, element) =>
+          element < previousValue ? element : previousValue,
+    );
+    final max = prices.fold(
+      double.negativeInfinity,
+      (previousValue, element) =>
+          element > previousValue ? element : previousValue,
+    );
+    final median = prices[prices.length ~/ 2];
+
+    return PriceSpan(min: min, max: max, avg: median);
   }
 }
--- lib/models/chart_data.dart
+++ lib/models/chart_data.dart
@@ -8,6 +8,7 @@ class ChartData with _$ChartData {
   const factory ChartData({
     required String title,
     required double value,
+    double? secondaryValue,
     @Default(false) bool isSelected,
     Color? color,
     Widget? alternativeTitle,
