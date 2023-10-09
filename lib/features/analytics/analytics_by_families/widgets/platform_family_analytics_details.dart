ad187ca7a5ed990c9038f565061d906cc30073c2
Philipp Guertler <phili.gue@hotmail.de>
2023-10-09 19:12:08 +0200

    moved game data utils to top level utils

--- lib/features/analytics/analytics_by_families/widgets/platform_family_analytics_details.dart
+++ lib/features/analytics/analytics_by_families/widgets/platform_family_analytics_details.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:pile_of_shame/features/analytics/analytics_by_families/utils/game_data.dart';
 import 'package:pile_of_shame/l10n/generated/app_localizations.dart';
 import 'package:pile_of_shame/models/game.dart';
 import 'package:pile_of_shame/providers/format_provider.dart';
 import 'package:pile_of_shame/utils/constants.dart';
+import 'package:pile_of_shame/utils/game_data.dart';
 import 'package:pile_of_shame/widgets/charts/default_bar_chart.dart';
 import 'package:pile_of_shame/widgets/charts/default_comparison_chart.dart';
 import 'package:pile_of_shame/widgets/charts/default_pie_chart.dart';
