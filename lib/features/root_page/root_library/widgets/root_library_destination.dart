e6c56e714483d4436f99df19e0d2a492ee2d92ed
Philipp Guertler <phili.gue@hotmail.de>
2023-10-15 18:25:38 +0200

    updated animation of library icon

--- lib/features/root_page/root_library/widgets/root_library_destination.dart
+++ lib/features/root_page/root_library/widgets/root_library_destination.dart
@@ -6,7 +6,21 @@ NavigationDestination rootLibraryDestination(BuildContext context) {
   return NavigationDestination(
     key: const ValueKey('root_library'),
     icon: const Icon(Icons.library_books_outlined),
-    selectedIcon: const Icon(Icons.library_books).animate().shake(),
+    selectedIcon: const Icon(Icons.library_books)
+        .animate()
+        .flipH(
+          begin: 0.0,
+          end: 1.0,
+          curve: Curves.easeInOut,
+          duration: 150.ms,
+        )
+        .then(delay: 50.ms)
+        .flipV(
+          begin: 0.0,
+          end: 1.0,
+          curve: Curves.easeInOut,
+          duration: 150.ms,
+        ),
     label: AppLocalizations.of(context)!.library,
   );
 }
