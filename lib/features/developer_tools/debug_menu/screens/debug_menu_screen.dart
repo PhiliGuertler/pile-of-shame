eec91d1a5289a20d7c980e44e56111ea3d3efa41
Philipp Guertler <phili.gue@hotmail.de>
2023-10-20 10:13:14 +0200

    removed obsolete debug features

--- lib/features/developer_tools/debug_menu/screens/debug_menu_screen.dart
+++ lib/features/developer_tools/debug_menu/screens/debug_menu_screen.dart
@@ -3,13 +3,8 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:pile_of_shame/features/developer_tools/debug_game_platform_icons/screens/debug_game_platform_icons_screen.dart';
-import 'package:pile_of_shame/features/developer_tools/debug_platform_family_cards/screens/debug_platform_family_cards_screen.dart';
-import 'package:pile_of_shame/features/developer_tools/debug_swipe_to_trigger/screens/debug_swipe_to_trigger_screen.dart';
-import 'package:pile_of_shame/models/assets.dart';
 import 'package:pile_of_shame/utils/constants.dart';
-import 'package:pile_of_shame/widgets/animated/animated_rolling_number.dart';
 import 'package:pile_of_shame/widgets/app_scaffold.dart';
-import 'package:pile_of_shame/widgets/image_list_tile.dart';
 import 'package:pile_of_shame/widgets/segmented_action_card.dart';
 
 Stream<int> randomIntStream() async* {
@@ -62,53 +57,7 @@ class DebugMenuScreen extends StatelessWidget {
                         );
                       },
                     ),
-                    SegmentedActionCardItem(
-                      leading: const Icon(Icons.sports_esports),
-                      title: const Text("Swipe to Trigger"),
-                      onTap: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) =>
-                                const DebugSwipeToTriggerScreen(),
-                          ),
-                        );
-                      },
-                    ),
-                    SegmentedActionCardItem(
-                      leading: const Icon(Icons.sports_esports),
-                      title: const Text("Platform-Family Cards"),
-                      onTap: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) =>
-                                const DebugPlatformFamilyCardsScreen(),
-                          ),
-                        );
-                      },
-                    ),
-                    SegmentedActionCardItem(
-                      title: AnimatedRollingNumber(
-                        value: snapshot.data ?? 0,
-                        duration: 350.ms,
-                      ),
-                      subtitle: Text((snapshot.data ?? 0).toStringAsFixed(0)),
-                      trailing: const SizedBox(),
-                    ),
                   ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(
-                    left: defaultPaddingX,
-                    right: defaultPaddingX,
-                    top: 16.0,
-                    bottom: 8.0,
-                  ),
-                  child: ImageListTile(
-                    imagePath: ImageAssets.controllerGameCube.value,
-                    heroTag: "GCN",
-                    title: const Text("Title"),
-                    subtitle: const Text("Subtitle"),
-                  ),
                 ),
               ],
             ),
