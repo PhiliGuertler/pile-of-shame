8d9f44fd8d093f0669308ae4f4cd23574bffc2d1
Philipp Guertler <phili.gue@hotmail.de>
2023-10-26 16:47:43 +0200

    updated app and splash icons

---
+++ scripts/generate_splash_and_app_icons.py
@@ -0,0 +1,20 @@
+import os
+import subprocess
+import shutil
+
+class terminalColors:
+    WARNING = '\033[93m'
+    INFO = '\033[94m'
+    END_COLOR = '\033[0m'
+
+# first of all, jump into this directory no matter where we actually started this script
+script_path = os.path.abspath(__file__)
+script_directory = os.path.dirname(script_path)
+os.chdir(script_directory)
+# now jump to the root directory of the repository
+os.chdir('..')
+
+# generate app icon
+subprocess.run(['dart', 'run', 'flutter_launcher_icons'], check=True, shell=True)
+# generate splash screens
+subprocess.run(['dart', 'run', 'flutter_native_splash:create'], check=True, shell=True)
