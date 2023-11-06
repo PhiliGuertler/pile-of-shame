fe719a15739e308278df6fd56878e3ad66cabe6f
Philipp Guertler <phili.gue@hotmail.de>
2023-11-06 13:01:39 +0100

    reworked number_input_field to use a different kind of input

--- lib/widgets/input/number_input_field.dart
+++ lib/widgets/input/number_input_field.dart
@@ -1,27 +1,8 @@
+import 'package:currency_textfield/currency_textfield.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/intl.dart';
 import 'package:pile_of_shame/providers/format_provider.dart';
-
-class DecimalInputFormatter extends TextInputFormatter {
-  final String decimalSeparator;
-  final String thousandsSeparator;
-
-  const DecimalInputFormatter({
-    required this.decimalSeparator,
-    required this.thousandsSeparator,
-  });
-
-  @override
-  TextEditingValue formatEditUpdate(
-    TextEditingValue oldValue,
-    TextEditingValue newValue,
-  ) {
-    final regex = RegExp('^\\d*\\$decimalSeparator?\\d{0,2}\$');
-    final String newString = regex.stringMatch(newValue.text) ?? "";
-    return newString == newValue.text ? newValue : oldValue;
-  }
-}
 
 class NumberInputField extends ConsumerWidget {
   final Widget label;
@@ -36,7 +17,6 @@ class NumberInputField extends ConsumerWidget {
   final String? Function(String? value)? validator;
 
   const NumberInputField({
-    super.key,
     required this.label,
     this.initialValue,
     this.textInputAction = TextInputAction.next,
@@ -49,46 +29,98 @@ class NumberInputField extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final numberFormatter = ref.watch(numberFormatProvider(context));
-    final decimalSeparator = numberFormatter.symbols.DECIMAL_SEP;
-    final thousandsSeparator = numberFormatter.symbols.GROUP_SEP;
-
-    final startValue = initialValue != null
-        ? numberFormatter
-            .format(initialValue)
-            .replaceAll(RegExp("\\$thousandsSeparator"), "")
-        : null;
-
-    return TextFormField(
-      keyboardType: const TextInputType.numberWithOptions(
-        decimal: true,
-      ),
-      decoration: InputDecoration(
-        label: label,
-        enabled: enabled,
-        helperText: helperText,
-        suffix: isCurrency ? const Text("€") : null,
-      ),
-      inputFormatters: [
-        DecimalInputFormatter(
-          decimalSeparator: decimalSeparator,
-          thousandsSeparator: thousandsSeparator,
-        ),
-        FilteringTextInputFormatter.allow(RegExp("[\\d$decimalSeparator]")),
-      ],
+    final numberFormatter = ref.watch(currencyFormatProvider(context));
+    return _InternalNumberInputField(
+      label: label,
+      enabled: enabled,
+      initialValue: initialValue,
       textInputAction: textInputAction,
-      initialValue: startValue,
-      onChanged: (textValue) {
-        if (onChanged != null) {
-          try {
-            final parsed = numberFormatter.parse(textValue);
-            onChanged!(parsed.toDouble());
-          } catch (error) {
-            onChanged!(0);
-          }
-        }
-      },
+      onChanged: onChanged,
+      helperText: helperText,
+      isCurrency: isCurrency,
       validator: validator,
+      numberFormatter: numberFormatter,
+    );
+  }
+}
+
+class _InternalNumberInputField extends ConsumerStatefulWidget {
+  final Widget label;
+  final double? initialValue;
+  final String? helperText;
+  final bool enabled;
+  final bool isCurrency;
+
+  /// Defaults to TextInputAction.next
+  final TextInputAction textInputAction;
+  final void Function(double value)? onChanged;
+  final String? Function(String? value)? validator;
+  final NumberFormat numberFormatter;
+
+  const _InternalNumberInputField({
+    required this.label,
+    required this.numberFormatter,
+    this.initialValue,
+    this.textInputAction = TextInputAction.next,
+    this.onChanged,
+    this.helperText,
+    this.enabled = true,
+    this.isCurrency = false,
+    this.validator,
+  });
+
+  @override
+  ConsumerState<_InternalNumberInputField> createState() =>
+      _NumberInputFieldState();
+}
+
+class _NumberInputFieldState extends ConsumerState<_InternalNumberInputField> {
+  late CurrencyTextFieldController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = CurrencyTextFieldController(
+      initDoubleValue: widget.initialValue ?? 0.0,
+      enableNegative: false,
+      currencyOnLeft: false,
+      decimalSymbol: widget.numberFormatter.symbols.DECIMAL_SEP,
+      thousandSymbol: widget.numberFormatter.symbols.GROUP_SEP,
+      currencySymbol: widget.numberFormatter.currencySymbol,
+    );
+    _controller.addListener(() {
+      String fieldContent = _controller.value.text;
+      fieldContent =
+          fieldContent.replaceAll(widget.numberFormatter.currencySymbol, "");
+      if (fieldContent.isEmpty) {
+        widget.onChanged?.call(0.0);
+      } else {
+        final value = widget.numberFormatter.parse(fieldContent.trim());
+        widget.onChanged?.call(value.toDouble());
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      textAlign: TextAlign.end,
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+        label: widget.label,
+        enabled: widget.enabled,
+        helperText: widget.helperText,
+        hintText: widget.numberFormatter.format(0),
+      ),
+      controller: _controller,
+      textInputAction: widget.textInputAction,
+      validator: widget.validator,
     );
   }
 }
