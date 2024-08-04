3f94775b713fc89373b4e5444fda29418afabc36
Philipp Guertler <phili.gue@hotmail.de>
2024-08-04 21:35:40 +0200

    expanded price variants by subscriptions

--- lib/models/price_variant.dart
+++ lib/models/price_variant.dart
@@ -6,36 +6,49 @@ enum PriceVariant {
     iconData: Icons.shopping_cart,
     backgroundColor: Color.fromRGBO(148, 228, 152, 1),
     foregroundColor: Colors.black,
+    hasPrice: true,
   ),
   borrowed(
     iconData: Icons.people,
     backgroundColor: Color.fromRGBO(223, 184, 136, 1),
     foregroundColor: Colors.black,
+    hasPrice: false,
   ),
   gifted(
     iconData: Icons.cake,
     backgroundColor: Color.fromRGBO(142, 231, 243, 1),
     foregroundColor: Colors.black,
+    hasPrice: false,
   ),
   observing(
     iconData: Icons.visibility,
     backgroundColor: Color.fromRGBO(222, 166, 238, 1),
     foregroundColor: Colors.black,
+    hasPrice: true,
   ),
   freeToPlay(
     iconData: Icons.play_circle,
     backgroundColor: Color.fromRGBO(238, 172, 205, 1),
     foregroundColor: Colors.black,
+    hasPrice: false,
+  ),
+  subscription(
+    iconData: Icons.subscriptions,
+    backgroundColor: Color.fromRGBO(240, 225, 144, 1),
+    foregroundColor: Colors.black,
+    hasPrice: false,
   );
 
   final Color foregroundColor;
   final Color backgroundColor;
   final IconData iconData;
+  final bool hasPrice;
 
   const PriceVariant({
     required this.foregroundColor,
     required this.backgroundColor,
     required this.iconData,
+    required this.hasPrice,
   });
 
   String toLocaleString(AppLocalizations l10n) {
@@ -50,6 +63,8 @@ enum PriceVariant {
         return l10n.observing;
       case PriceVariant.freeToPlay:
         return l10n.freeToPlay;
+      case PriceVariant.subscription:
+        return l10n.subscription;
     }
   }
 }
